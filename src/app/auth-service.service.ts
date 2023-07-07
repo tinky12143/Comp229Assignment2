cd0d4a0257f1f501cb977ea0e34f13fcfc7b7a34
tinky12143 <tinky12143@outlook.com>
2023-07-07 17:28:39 -0400

    heroku11

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -18,7 +18,7 @@ export class AuthServiceService {
 
   loginAccount(name: string, password: string) {
     this.httpClient
-      .post<{ message: string }>('/login', {
+      .post<{ message: string }>('/credential', {
         name: name,
         password: password,
       })
@@ -30,7 +30,7 @@ export class AuthServiceService {
           this.checkLogin.next(true);
           this.router.navigate(['/business-page']);
         } else {
-          this.router.navigate(['/login']);
+          this.router.navigate(['/credential']);
         }
       });
   }
